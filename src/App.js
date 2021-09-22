3edbec1015c6639e5ec5818145e9d8aa78b459c6
Radu <rau_radu@yahoo.com>
2021-09-22 17:43:24 +0300

    altered the src/App.js file to indicate that this brach was changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ba nene ba! debandata asta 
+         This is the feature branch
         </p>
         <a
           className="App-link"
